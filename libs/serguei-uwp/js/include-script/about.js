926abbe5f419c881d38e5b990464d5211f719438
englishextra <englishextra@yandex.ru>
2018-11-29 23:52:54 +0300

    updates

--- libs/serguei-uwp/js/include-script/about.js
+++ libs/serguei-uwp/js/include-script/about.js
@@ -1,148 +1,143 @@
-"use strict";
-
 /*global console, GLightbox, imagesLoaded, LazyLoad, manageExternalLinkAll,
 manageMacy, manageReadMore, updateMacyThrottled*/
-
 /*!
  * page logic
  */
 (function (root, document) {
-  "use strict";
+	"use strict";
 
-  var run = function run() {
-    var classList = "classList";
-    var getElementsByClassName = "getElementsByClassName";
-    var querySelectorAll = "querySelectorAll";
-    var _addEventListener = "addEventListener";
-    var _length = "length";
-    var glightboxClass = "glightbox";
-    /*!
-     * @see {@link https://glightbox.mcstudios.com.mx/#options}
-     */
+	var run = function () {
 
-    var manageGlightbox = function manageGlightbox(glightboxClass) {
-      if (root.GLightbox) {
-        var glightbox;
-        glightbox = GLightbox({
-          selector: glightboxClass
-        });
-      }
-    };
+		var classList = "classList";
+		var getElementsByClassName = "getElementsByClassName";
+		var querySelectorAll = "querySelectorAll";
+		var _addEventListener = "addEventListener";
+		var _length = "length";
 
-    var dataSrcLazyClass = "data-src-lazy";
-    /*!
-     * @see {@link https://github.com/verlok/lazyload}
-     */
+		var glightboxClass = "glightbox";
 
-    var manageLazyLoad = function manageLazyLoad(dataSrcLazyClass) {
-      if (root.LazyLoad) {
-        var lzld;
-        lzld = new LazyLoad({
-          elements_selector: "." + dataSrcLazyClass
-        });
-      }
-    };
-    /*!
-     * @see {@link https://imagesloaded.desandro.com/}
-     * Triggered after all images have been either loaded or confirmed broken.
-     */
+		/*!
+		 * @see {@link https://glightbox.mcstudios.com.mx/#options}
+		 */
+		var manageGlightbox = function (glightboxClass) {
+			if (root.GLightbox) {
+				var glightbox;
+				glightbox = GLightbox({
+						selector: glightboxClass
+					});
+			}
+		};
 
+		var dataSrcLazyClass = "data-src-lazy";
 
-    var onImagesLoaded = function onImagesLoaded(macyGrid) {
-      if (root.imagesLoaded) {
-        var imgLoad;
-        imgLoad = new imagesLoaded(macyGrid);
+		/*!
+		 * @see {@link https://github.com/verlok/lazyload}
+		 */
+		var manageLazyLoad = function (dataSrcLazyClass) {
+			if (root.LazyLoad) {
+				var lzld;
+				lzld = new LazyLoad({
+						elements_selector: "." + dataSrcLazyClass
+					});
+			}
+		};
 
-        var onAlways = function onAlways(instance) {
-          if (root.updateMacyThrottled) {
-            updateMacyThrottled();
-          }
+		/*!
+		 * @see {@link https://imagesloaded.desandro.com/}
+		 * Triggered after all images have been either loaded or confirmed broken.
+		 */
+		var onImagesLoaded = function (macyGrid) {
+			if (root.imagesLoaded) {
+				var imgLoad;
+				imgLoad = new imagesLoaded(macyGrid);
+				var onAlways = function (instance) {
+					if (root.updateMacyThrottled) {
+						updateMacyThrottled();
+					}
+					console.log("imagesLoaded: found " + instance.images[_length] + " images");
+				};
+				imgLoad.on("always", onAlways);
+			}
+		};
 
-          console.log("imagesLoaded: found " + instance.images[_length] + " images");
-        };
+		var isBindedMacyItemClass = "is-binded-macy-item";
 
-        imgLoad.on("always", onAlways);
-      }
-    };
+		var macyGridClass = "macy-grid";
 
-    var isBindedMacyItemClass = "is-binded-macy-item";
-    var macyGridClass = "macy-grid";
-    var macyGrid = document[getElementsByClassName](macyGridClass)[0] || "";
+		var macyGrid = document[getElementsByClassName](macyGridClass)[0] || "";
 
-    var onMacyRender = function onMacyRender() {
-      updateMacyThrottled();
-      onImagesLoaded(macyGrid);
-      manageLazyLoad(dataSrcLazyClass);
-      manageExternalLinkAll();
-      manageGlightbox(glightboxClass);
-      manageReadMore(null, {
-        target: ".dummy",
-        numOfWords: 10,
-        toggle: true,
-        moreLink: "БОЛЬШЕ",
-        lessLink: "МЕНЬШЕ",
-        inline: true,
-        customBlockElement: "p"
-      });
-    };
+		var onMacyRender = function () {
+			updateMacyThrottled();
+			onImagesLoaded(macyGrid);
+			manageLazyLoad(dataSrcLazyClass);
+			manageExternalLinkAll();
+			manageGlightbox(glightboxClass);
+			manageReadMore(null, {
+				target: ".dummy",
+				numOfWords: 10,
+				toggle: true,
+				moreLink: "БОЛЬШЕ",
+				lessLink: "МЕНЬШЕ",
+				inline: true,
+				customBlockElement: "p"
+			});
+		};
 
-    var onMacyResize = function onMacyResize() {
-      try {
-        var container = macyGrid ? macyGrid.children || macyGrid[querySelectorAll]("." + macyGridClass + " > *") || "" : "";
+		var onMacyResize = function () {
+			try {
+				var container = macyGrid ? (macyGrid.children || macyGrid[querySelectorAll]("." + macyGridClass + " > *") || "") : "";
+				if (container) {
+					var i,
+					l;
+					for (i = 0, l = container[_length]; i < l; i += 1) {
+						if (!container[i][classList].contains(isBindedMacyItemClass)) {
+							container[i][classList].add(isBindedMacyItemClass);
+							container[i][_addEventListener]("onresize", updateMacyThrottled, {
+								passive: true
+							});
+						}
+					}
+					i = l = null;
+				}
+			} catch (err) {
+				throw new Error("cannot onMacyResize " + err);
+			}
+		};
 
-        if (container) {
-          var i, l;
+		var onMacyManage = function () {
+			onMacyRender();
+			onMacyResize();
+		};
 
-          for (i = 0, l = container[_length]; i < l; i += 1) {
-            if (!container[i][classList].contains(isBindedMacyItemClass)) {
-              container[i][classList].add(isBindedMacyItemClass);
+		var addMacyItems = function (macyGrid, callback) {
+			if ("function" === typeof callback) {
+				callback();
+			}
+		};
 
-              container[i][_addEventListener]("onresize", updateMacyThrottled, {
-                passive: true
-              });
-            }
-          }
+		if (macyGrid) {
+			manageMacy(macyGridClass, {
+				trueOrder: false,
+				waitForImages: false,
+				margin: 20,
+				columns: 3,
+				breakAt: {
+					1280: 2,
+					1024: 2,
+					960: 2,
+					640: 1,
+					480: 1,
+					360: 1
+				}
+			});
 
-          i = l = null;
-        }
-      } catch (err) {
-        throw new Error("cannot onMacyResize " + err);
-      }
-    };
+			addMacyItems(macyGrid, onMacyManage);
+		}
 
-    var onMacyManage = function onMacyManage() {
-      onMacyRender();
-      onMacyResize();
-    };
+		if (root.manageExternalLinkAll) {
+			manageExternalLinkAll();
+		}
+	};
+	run();
 
-    var addMacyItems = function addMacyItems(macyGrid, callback) {
-      if ("function" === typeof callback) {
-        callback();
-      }
-    };
-
-    if (macyGrid) {
-      manageMacy(macyGridClass, {
-        trueOrder: false,
-        waitForImages: false,
-        margin: 20,
-        columns: 3,
-        breakAt: {
-          1280: 2,
-          1024: 2,
-          960: 2,
-          640: 1,
-          480: 1,
-          360: 1
-        }
-      });
-      addMacyItems(macyGrid, onMacyManage);
-    }
-
-    if (root.manageExternalLinkAll) {
-      manageExternalLinkAll();
-    }
-  };
-
-  run();
-})("undefined" !== typeof window ? window : void 0, document);
+})("undefined" !== typeof window ? window : this, document);
--- libs/serguei-uwp/js/include-script/works.js
+++ libs/serguei-uwp/js/include-script/works.js
@@ -1,207 +1,200 @@
-"use strict";
-
 /*global console, imagesLoaded, LazyLoad, manageExternalLinkAll, manageMacy,
 updateMacyThrottled*/
-
 /*!
  * page logic
  */
 (function (root, document) {
-  "use strict";
+	"use strict";
 
-  var run = function run() {
-    var appendChild = "appendChild";
-    var classList = "classList";
-    var getElementsByClassName = "getElementsByClassName";
-    var querySelectorAll = "querySelectorAll";
-    var setAttribute = "setAttribute";
-    var _addEventListener = "addEventListener";
-    var _length = "length";
-    var dataSrcLazyClass = "data-src-lazy";
-    /*!
-     * @see {@link https://github.com/verlok/lazyload}
-     */
+	var run = function () {
 
-    var manageLazyLoad = function manageLazyLoad(dataSrcLazyClass) {
-      if (root.LazyLoad) {
-        var lzld;
-        lzld = new LazyLoad({
-          elements_selector: "." + dataSrcLazyClass
-        });
-      }
-    };
-    /*!
-     * @see {@link https://imagesloaded.desandro.com/}
-     * Triggered after all images have been either loaded or confirmed broken.
-     */
+		var appendChild = "appendChild";
+		var classList = "classList";
+		var getElementsByClassName = "getElementsByClassName";
+		var querySelectorAll = "querySelectorAll";
+		var setAttribute = "setAttribute";
+		var _addEventListener = "addEventListener";
+		var _length = "length";
 
+		var dataSrcLazyClass = "data-src-lazy";
 
-    var onImagesLoaded = function onImagesLoaded(macyGrid) {
-      if (root.imagesLoaded) {
-        var imgLoad;
-        imgLoad = new imagesLoaded(macyGrid);
+		/*!
+		 * @see {@link https://github.com/verlok/lazyload}
+		 */
+		var manageLazyLoad = function (dataSrcLazyClass) {
+			if (root.LazyLoad) {
+				var lzld;
+				lzld = new LazyLoad({
+						elements_selector: "." + dataSrcLazyClass
+					});
+			}
+		};
 
-        var onAlways = function onAlways(instance) {
-          if (root.updateMacyThrottled) {
-            updateMacyThrottled();
-          }
+		/*!
+		 * @see {@link https://imagesloaded.desandro.com/}
+		 * Triggered after all images have been either loaded or confirmed broken.
+		 */
+		var onImagesLoaded = function (macyGrid) {
+			if (root.imagesLoaded) {
+				var imgLoad;
+				imgLoad = new imagesLoaded(macyGrid);
+				var onAlways = function (instance) {
+					if (root.updateMacyThrottled) {
+						updateMacyThrottled();
+					}
+					console.log("imagesLoaded: found " + instance.images[_length] + " images");
+				};
+				imgLoad.on("always", onAlways);
+			}
+		};
 
-          console.log("imagesLoaded: found " + instance.images[_length] + " images");
-        };
+		var isBindedMacyItemClass = "is-binded-macy-item";
 
-        imgLoad.on("always", onAlways);
-      }
-    };
+		var macyGridClass = "macy-grid";
 
-    var isBindedMacyItemClass = "is-binded-macy-item";
-    var macyGridClass = "macy-grid";
-    var macyGrid = document[getElementsByClassName](macyGridClass)[0] || "";
+		var macyGrid = document[getElementsByClassName](macyGridClass)[0] || "";
 
-    var onMacyRender = function onMacyRender() {
-      updateMacyThrottled();
-      onImagesLoaded(macyGrid);
-      manageLazyLoad(dataSrcLazyClass);
-      manageExternalLinkAll();
-    };
+		var onMacyRender = function () {
+			updateMacyThrottled();
+			onImagesLoaded(macyGrid);
+			manageLazyLoad(dataSrcLazyClass);
+			manageExternalLinkAll();
+		};
 
-    var onMacyResize = function onMacyResize() {
-      try {
-        var container = macyGrid ? macyGrid.children || macyGrid[querySelectorAll]("." + macyGridClass + " > *") || "" : "";
+		var onMacyResize = function () {
+			try {
+				var container = macyGrid ? (macyGrid.children || macyGrid[querySelectorAll]("." + macyGridClass + " > *") || "") : "";
+				if (container) {
+					var i,
+					l;
+					for (i = 0, l = container[_length]; i < l; i += 1) {
+						if (!container[i][classList].contains(isBindedMacyItemClass)) {
+							container[i][classList].add(isBindedMacyItemClass);
+							container[i][_addEventListener]("onresize", updateMacyThrottled, {
+								passive: true
+							});
+						}
+					}
+					i = l = null;
+				}
+			} catch (err) {
+				throw new Error("cannot onMacyResize " + err);
+			}
+		};
 
-        if (container) {
-          var i, l;
+		var onMacyManage = function () {
+			onMacyRender();
+			onMacyResize();
+		};
 
-          for (i = 0, l = container[_length]; i < l; i += 1) {
-            if (!container[i][classList].contains(isBindedMacyItemClass)) {
-              container[i][classList].add(isBindedMacyItemClass);
+		var macyItems = [{
+				"href": "https://build.phonegap.com/apps/1824701/share",
+				"src": "./libs/serguei-uwp/img/works-screenshots/@1x/build.phonegap.com-apps-1824701-share.jpg"
+			}, {
+				"href": "https://englishextra.github.io/403.html",
+				"src": "./libs/serguei-uwp/img/works-screenshots/@1x/englishextra.github.io-403-html.jpg"
+			}, {
+				"href": "https://englishextra.github.io/404.html",
+				"src": "./libs/serguei-uwp/img/works-screenshots/@1x/englishextra.github.io-404-html.jpg"
+			}, {
+				"href": "https://englishextra.github.io/pages/more/more_irregular_verbs.html",
+				"src": "./libs/serguei-uwp/img/works-screenshots/@1x/englishextra.github.io-pages-more-more_irregular_verbs-html.jpg"
+			}, {
+				"href": "https://englishextra.github.io/pages/more/more_newsletter_can_get_english_for_free.html",
+				"src": "./libs/serguei-uwp/img/works-screenshots/@1x/englishextra.github.io-pages-more-more_newsletter_can_get_english_for_free-html.jpg"
+			}, {
+				"href": "https://englishextra.github.io/serguei/about.html",
+				"src": "./libs/serguei-uwp/img/works-screenshots/@1x/englishextra.github.io-serguei-about-html.jpg"
+			}, {
+				"href": "https://englishextra.github.io/serguei/slides.html",
+				"src": "./libs/serguei-uwp/img/works-screenshots/@1x/englishextra.github.io-serguei-slides-html.jpg"
+			}, {
+				"href": "https://englishextra.github.io/",
+				"src": "./libs/serguei-uwp/img/works-screenshots/@1x/englishextra.github.io.jpg"
+			}, {
+				"href": "https://englishextra.gitlab.io/",
+				"src": "./libs/serguei-uwp/img/works-screenshots/@1x/englishextra.gitlab.io.jpg"
+			}, {
+				"href": "https://github.com/englishextra",
+				"src": "./libs/serguei-uwp/img/works-screenshots/@1x/github.com-englishextra.jpg"
+			}, {
+				"href": "https://mytushino.github.io/",
+				"src": "./libs/serguei-uwp/img/works-screenshots/@1x/mytushino.github.io.jpg"
+			}, {
+				"href": "https://noushevr.github.io/",
+				"src": "./libs/serguei-uwp/img/works-screenshots/@1x/noushevr.github.io.jpg"
+			}, {
+				"href": "https://www.behance.net/englishextra",
+				"src": "./libs/serguei-uwp/img/works-screenshots/@1x/www.behance.net-englishextra.jpg"
+			}, {
+				"href": "https://www.domestika.org/en/englishextra",
+				"src": "./libs/serguei-uwp/img/works-screenshots/@1x/www.domestika.org-en-englishextra.jpg"
+			}, {
+				"href": "https://www.npmjs.com/~englishextra",
+				"src": "./libs/serguei-uwp/img/works-screenshots/@1x/www.npmjs.com-englishextra.jpg"
+			}
+		];
 
-              container[i][_addEventListener]("onresize", updateMacyThrottled, {
-                passive: true
-              });
-            }
-          }
+		var addMacyItems = function (macyGrid, callback) {
+			var dataSrcImgKeyName = "src";
+			var transparentPixel = "data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns%3D%27http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%27%20viewBox%3D%270%200%201%201%27%2F%3E";
+			/* var html = "";
+			var i,
+			l;
+			for (i = 0, l = macyItems[_length]; i < l; i += 1) {
+				html += '<a href="' + macyItems[i].href + '" aria-label="Ссылка"><img src="' + transparentPixel + '" class="' + dataSrcImgClass + '" data-' + dataSrcImgKeyName + '="' + macyItems[i].src + '" alt="" /></a>\n';
+			}
+			i = l = null;
+			macyGrid.innerHTML = html;
+			if ("function" === typeof callback) {
+				callback();
+			} */
+			var count = 0;
+			var i,
+			l;
+			for (i = 0, l = macyItems[_length]; i < l; i += 1) {
+				var a = document.createElement("a");
+				a[setAttribute]("href", macyItems[i].href);
+				a[setAttribute]("aria-label", "Ссылка");
+				var img = document.createElement("img");
+				a[appendChild](img);
+				img[setAttribute]("src", transparentPixel);
+				img[setAttribute]("class", dataSrcLazyClass);
+				img[setAttribute]("data-" + dataSrcImgKeyName, macyItems[i].src);
+				macyGrid[appendChild](a);
+				count++;
+				if (count === macyItems[_length]) {
+					if ("function" === typeof callback) {
+						callback();
+					}
+				}
+			}
+			i = l = null;
+		};
 
-          i = l = null;
-        }
-      } catch (err) {
-        throw new Error("cannot onMacyResize " + err);
-      }
-    };
+		if (macyGrid) {
+			manageMacy(macyGridClass, {
+				trueOrder: false,
+				waitForImages: false,
+				margin: 0,
+				columns: 4,
+				breakAt: {
+					1280: 4,
+					1024: 3,
+					960: 2,
+					640: 2,
+					480: 1,
+					360: 1
+				}
+			});
 
-    var onMacyManage = function onMacyManage() {
-      onMacyRender();
-      onMacyResize();
-    };
+			addMacyItems(macyGrid, onMacyManage);
+		}
 
-    var macyItems = [{
-      "href": "https://build.phonegap.com/apps/1824701/share",
-      "src": "./libs/serguei-uwp/img/works-screenshots/@1x/build.phonegap.com-apps-1824701-share.jpg"
-    }, {
-      "href": "https://englishextra.github.io/403.html",
-      "src": "./libs/serguei-uwp/img/works-screenshots/@1x/englishextra.github.io-403-html.jpg"
-    }, {
-      "href": "https://englishextra.github.io/404.html",
-      "src": "./libs/serguei-uwp/img/works-screenshots/@1x/englishextra.github.io-404-html.jpg"
-    }, {
-      "href": "https://englishextra.github.io/pages/more/more_irregular_verbs.html",
-      "src": "./libs/serguei-uwp/img/works-screenshots/@1x/englishextra.github.io-pages-more-more_irregular_verbs-html.jpg"
-    }, {
-      "href": "https://englishextra.github.io/pages/more/more_newsletter_can_get_english_for_free.html",
-      "src": "./libs/serguei-uwp/img/works-screenshots/@1x/englishextra.github.io-pages-more-more_newsletter_can_get_english_for_free-html.jpg"
-    }, {
-      "href": "https://englishextra.github.io/serguei/about.html",
-      "src": "./libs/serguei-uwp/img/works-screenshots/@1x/englishextra.github.io-serguei-about-html.jpg"
-    }, {
-      "href": "https://englishextra.github.io/serguei/slides.html",
-      "src": "./libs/serguei-uwp/img/works-screenshots/@1x/englishextra.github.io-serguei-slides-html.jpg"
-    }, {
-      "href": "https://englishextra.github.io/",
-      "src": "./libs/serguei-uwp/img/works-screenshots/@1x/englishextra.github.io.jpg"
-    }, {
-      "href": "https://englishextra.gitlab.io/",
-      "src": "./libs/serguei-uwp/img/works-screenshots/@1x/englishextra.gitlab.io.jpg"
-    }, {
-      "href": "https://github.com/englishextra",
-      "src": "./libs/serguei-uwp/img/works-screenshots/@1x/github.com-englishextra.jpg"
-    }, {
-      "href": "https://mytushino.github.io/",
-      "src": "./libs/serguei-uwp/img/works-screenshots/@1x/mytushino.github.io.jpg"
-    }, {
-      "href": "https://noushevr.github.io/",
-      "src": "./libs/serguei-uwp/img/works-screenshots/@1x/noushevr.github.io.jpg"
-    }, {
-      "href": "https://www.behance.net/englishextra",
-      "src": "./libs/serguei-uwp/img/works-screenshots/@1x/www.behance.net-englishextra.jpg"
-    }, {
-      "href": "https://www.domestika.org/en/englishextra",
-      "src": "./libs/serguei-uwp/img/works-screenshots/@1x/www.domestika.org-en-englishextra.jpg"
-    }, {
-      "href": "https://www.npmjs.com/~englishextra",
-      "src": "./libs/serguei-uwp/img/works-screenshots/@1x/www.npmjs.com-englishextra.jpg"
-    }];
+		if (root.manageExternalLinkAll) {
+			manageExternalLinkAll();
+		}
+	};
+	run();
 
-    var addMacyItems = function addMacyItems(macyGrid, callback) {
-      var dataSrcImgKeyName = "src";
-      var transparentPixel = "data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns%3D%27http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%27%20viewBox%3D%270%200%201%201%27%2F%3E";
-      /* var html = "";
-      var i,
-      l;
-      for (i = 0, l = macyItems[_length]; i < l; i += 1) {
-      	html += '<a href="' + macyItems[i].href + '" aria-label="Ссылка"><img src="' + transparentPixel + '" class="' + dataSrcImgClass + '" data-' + dataSrcImgKeyName + '="' + macyItems[i].src + '" alt="" /></a>\n';
-      }
-      i = l = null;
-      macyGrid.innerHTML = html;
-      if ("function" === typeof callback) {
-      	callback();
-      } */
-
-      var count = 0;
-      var i, l;
-
-      for (i = 0, l = macyItems[_length]; i < l; i += 1) {
-        var a = document.createElement("a");
-        a[setAttribute]("href", macyItems[i].href);
-        a[setAttribute]("aria-label", "Ссылка");
-        var img = document.createElement("img");
-        a[appendChild](img);
-        img[setAttribute]("src", transparentPixel);
-        img[setAttribute]("class", dataSrcLazyClass);
-        img[setAttribute]("data-" + dataSrcImgKeyName, macyItems[i].src);
-        macyGrid[appendChild](a);
-        count++;
-
-        if (count === macyItems[_length]) {
-          if ("function" === typeof callback) {
-            callback();
-          }
-        }
-      }
-
-      i = l = null;
-    };
-
-    if (macyGrid) {
-      manageMacy(macyGridClass, {
-        trueOrder: false,
-        waitForImages: false,
-        margin: 0,
-        columns: 4,
-        breakAt: {
-          1280: 4,
-          1024: 3,
-          960: 2,
-          640: 2,
-          480: 1,
-          360: 1
-        }
-      });
-      addMacyItems(macyGrid, onMacyManage);
-    }
-
-    if (root.manageExternalLinkAll) {
-      manageExternalLinkAll();
-    }
-  };
-
-  run();
-})("undefined" !== typeof window ? window : void 0, document);
+})("undefined" !== typeof window ? window : this, document);
