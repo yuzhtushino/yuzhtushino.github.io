dcf7c35847b3c49879ef3d2ed8772121caa58d1c
englishextra <englishextra@yandex.ru>
2018-12-24 15:28:37 +0300

    updates

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+/*global require */
+/*!
+ * @see {@link https://github.com/mildrenben/surface/blob/master/gulpfile.js}
+ * @see {@link https://www.webstoemp.com/blog/gulp-setup/}
+ * @see {@link https://gulpjs.com/plugins/blackList.json}
+ * @see {@link https://hackernoon.com/how-to-automate-all-the-things-with-gulp-b21a3fc96885}
+ * @see {@link https://stackoverflow.com/questions/36897877/gulp-error-the-following-tasks-did-not-complete-did-you-forget-to-signal-async}
+ * @see {@link https://zzz.buzz/2016/11/19/gulp-4-0-upgrade-guide/}
+ * @see {@link https://blog.khophi.co/migrate-gulp-4-complete-example/}
+ * @see {@link https://www.joezimjs.com/javascript/complete-guide-upgrading-gulp-4/}
+ * @see {@link https://codeburst.io/switching-to-gulp-4-0-271ae63530c0}
+ */
+
+var gulp = require("gulp");
+var eslint = require("gulp-eslint");
+
+var options = {
+	libPaths: {
+		src: "**/libs/**/src/*.js"
+	},
+};
+
+gulp.task("lint-js", function () {
+	return gulp.src(options.libPaths.src)
+	.pipe(eslint())
+	.pipe(eslint.format())
+	.pipe(eslint.failAfterError());
+});
+
+gulp.task("default", gulp.task("lint-js"));
